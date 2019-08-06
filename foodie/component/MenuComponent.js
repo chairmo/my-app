38cb3fd575e5dfe33cbba2b49463b6defa7f3591
chairmo <iyungmoni@gmail.com>
2019-08-06 10:29:50 +0100

    Component part 1

---
+++ foodie/component/MenuComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, FlatList} from 'react-native';
+import {ListItem} from 'react-native-elements';
+
+function Menu(props) {
+
+    const renderMenuItem = ({item, index}) => {
+        return(
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                leftAvater={{rounded:true, source: require('./images/buffet.png')}}
+            />
+        )
+    };
+
+    return(
+        <FlatList
+            data={props.dishes}
+            renderItem={renderMenuItem}
+            keyEstractor={item=> item.id.toString()}/>
+    )
+}
+
+export default Menu;
